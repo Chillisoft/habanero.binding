b58bdcf1c251c62ed7589f32e03d30b7b802e5d3
Chillisoft <support@chillisoft.co.za>
2011-01-19 10:26:30 +0000

    Added a AssemblyInfoShared.cs
    git-svn-id: http://delicious:8080/svn/habanero/HabaneroCommunity/Habanero.Binding/trunk@15166 6b8f5710-b20c-734b-ac0a-461ee9249d7d

--- source/Habanero.Binding/Properties/AssemblyInfo.cs
+++ source/Habanero.Binding/Properties/AssemblyInfo.cs
@@ -8,11 +8,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Habanero.Binding")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Chillisoft Pty (Ltd)")]
-[assembly: AssemblyProduct("Habanero.Binding")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2010")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -22,15 +17,3 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("1e305a33-a9e4-46f9-bcde-a58f3dbb1252")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
