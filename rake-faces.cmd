e4d8f8aa4136aa1c5340b33ba9801e5d0239dd50
Brett Powell <brett.powell@chillisoft.co.za>
2011-05-16 17:59:40 +0000

    removed unused csproj's
    git-svn-id: http://delicious:8080/svn/habanero/HabaneroCommunity/Habanero.Binding/branches/v1.2_ForCF_Stargate@19162 6b8f5710-b20c-734b-ac0a-461ee9249d7d

--- rake-faces.cmd
+++
@@ -1,3 +0,0 @@
-@cd temp/faces 
-@rmdir /s /q .svn 
-rake --rakefile facesCF-library-rakefile.rb --execute-continue "$faces_version = 'branches/V2.6-CF_Stargate'; $buildscriptpath = 'F:/Systems/HabaneroCommunity/BuildScripts'" 
